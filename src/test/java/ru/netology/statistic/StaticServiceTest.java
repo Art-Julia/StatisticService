0935ed63687ccdaaa167e6c9bf1e0ca6adadea14
Yulia Arutyunyan <artn.julia@gmail.com>
2021-06-03 23:22:02 +0300

    Initial commit

---
+++ src/test/java/ru/netology/statistic/StaticServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StaticServiceTest {
+
+    //@org.junit.jupiter.api.Test
+    @ParameterizedTest
+    void minSales() {
+       
+
+    }
+
+   // @org.junit.jupiter.api.Test
+   @Test
+    void maxSales() {
+    }
+}
