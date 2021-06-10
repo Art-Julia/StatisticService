e897ab19d47cd65126f29d2ce6aa25076d7a12d7
Yulia Arutyunyan <artn.julia@gmail.com>
2021-06-10 20:35:28 +0300

    StaticService Test_final

--- src/main/java/ru/netology/statistic/StaticService.java
+++ src/main/java/ru/netology/statistic/StaticService.java
@@ -3,14 +3,12 @@ package ru.netology.statistic;
 public class StaticService {
     public int minSales(long[] sales) {
         int minMonth = 0;
-        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        int month = 0;
         for (long sale : sales) {
-            // sales[minMonth] - продажи в месяце minMonth
-            // sale - продажи в рассматриваемом месяце
             if (sale <= sales[minMonth]) {
                 minMonth = month;
             }
-            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+            month = month + 1;
         }
         return minMonth + 1;
     }
@@ -26,4 +24,45 @@ public class StaticService {
         }
         return maxMonth + 1;
     }
-}
+
+
+    public long calculateSumSales(long[] sales) {
+        long sumSales = 0;
+        for (long sale : sales) {
+            sumSales += sale;
+        }
+        return sumSales;
+    }
+
+    public long averageSales(long[] sales) {
+        long sumForAverage = calculateSumSales(sales);
+        return sumForAverage / sales.length;
+    }
+
+    public int aboveAverageSales(long[] sales) {
+        long averageMonthlySales = averageSales(sales);
+        int monthAboveAverage = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale > averageMonthlySales) {
+                monthAboveAverage++;
+            }
+            month = month + 1;
+        }
+        return monthAboveAverage;
+    }
+
+
+    public int belowAverageSales(long[] sales) {
+        long averageMonthlySales = averageSales(sales);
+        int monthBelowAverage = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale < averageMonthlySales) {
+                monthBelowAverage++;
+            }
+            month = month + 1;
+        }
+            return monthBelowAverage;
+        }
+    }
